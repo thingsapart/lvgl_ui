ac3269ec205b773d14ada7d3dab1dc9ef70cdfa0
thingsapart <thingstakenapart@gmail.com>
2025-06-25 08:40:59 -0700

    Fix run-tests and setup.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #/bin/sh
+
+# Init git submodules.
 git submodule update --init --depth 1
+
+# Install Linux packages.
+if command -v apt-get &>/dev/null; then
+  sudo apt-get update && sudo apt-get install -y libsdl2-dev doxygen
+fi
+
+# Install python deps.
+pip install pycparser
+
+# Build.
 cd cJSON; make
 cd -
 sh ./build_lvgl.sh
