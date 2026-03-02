7645c61dcaf23b7efe873cdec3621ceeee6d386a
thingsapart <thingstakenapart@gmail.com>
2026-03-02 00:06:32 -0800

    Fix typo

--- obj_registry.c
+++ obj_registry.c
@@ -1,5 +1,5 @@
 #include "obj_registry.h"
-#include "lvgl_ui_tils.h" // For print_warning
+#include "lvgl_ui_utils.h" // For print_warning
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
